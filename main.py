e5f9262d832a721097f653401bc47f2f11f21340
RedDragon-11 <mgaddis97@gmail.com>
2023-10-30 17:43:34 -0400

    Added Specials Function
    -Added function to Draw 2, Draw 4, Skip and Reverse (I haven't tested functionality yet, just added the logic that MIGHT work)
    
    I didn't have a whole lot of time to test this due to school work, will be testing game soon to see if everything works.

--- main.py
+++ main.py
@@ -7,52 +7,74 @@ centerCard = None
 playerHand = []
 cpuHand = []
 gameOver = False
+cpuSkipped = False
+playerSkipped = False
 
 deck = UNOcard.deckBuilder()
 playerHand = UNOlogic.drawStartingHand(deck)
-cpuHand = (UNOlogic.drawStartingHand(deck)) 
+cpuHand = UNOlogic.drawStartingHand(deck)
 centerCard = UNOlogic.setCenterCard(deck)
 
 
 #Loop game till someone has no cards in their hand.
 while (gameOver == False):
     UNOcard.displayHand(playerHand)
-    UNOcard.displayHand(cpuHand)
     UNOlogic.displayCenter(centerCard)
-    output = UNOlogic.play(deck, centerCard, playerHand, cpuHand)
-    newCenter = output[0]
-    newHand = output[1]
 
-    if (newCenter != None):
-        centerCard = newCenter
-        if(UNOcard.getSpecial(newCenter) ==  ug.DRAW2):
-            print("You've +2'd the Computer!")
-            for num in range(0,2):
-                cpuHand = UNOlogic.draw(deck, cpuHand)
-        elif(UNOcard.getSpecial(newCenter) ==  ug.WILD4):
-            print("You've +4'd the Computer!!")
-            for num in range(0,4):
-                cpuHand = UNOlogic.draw(deck, cpuHand)        
+    #output formated as [newCenter, newHand]
+    #This section gets the results of what the player plays, makes it the center card and removes the card from the player's hand
+    #the if statements check if the player drew a card, if they did just set the new hand. if they didn't check to see if the card that was played was a draw 2 or draw 4
+    #Realistically, I could have made that a function but I got lazy. Same for the cpu section.
+    if(playerSkipped == False):
+        output = UNOlogic.play(deck, centerCard, playerHand, cpuHand)
+        newCenter = output[0]
+        newHand = output[1]
+        if (newCenter != None):
+            centerCard = newCenter
+            if(UNOcard.getSpecial(newCenter) ==  ug.DRAW2):
+                print("You've +2'd the Computer!")
+                for num in range(0,2):
+                    cpuHand = UNOlogic.draw(deck, cpuHand)
+            elif(UNOcard.getSpecial(newCenter) ==  ug.WILD4):
+                print("You've +4'd the Computer!!")
+                for num in range(0,4):
+                    cpuHand = UNOlogic.draw(deck, cpuHand)  
+            #In 1v1 Uno, a reverse is effectively a skip since it makes the turn order come back to you. At Least that my understand. 
+            #Its either an effect skip or a card that has no effect. I opted in for it being another form of a skip
+            elif(UNOcard.getSpecial(newCenter) == ug.SKIP or UNOcard.getSpecial(newCenter) == ug.REVERSE): 
+                    print("The Computer's turn has been skipped!")
+                    cpuSkipped == True
+        palyerHand = newHand
+    elif(playerSkipped == True):
+        playerSkipped == False
+
     
-    palyerHand = newHand
+   
+    #output formated as [newCenter, newHand]
+    #This section gets the results of what the cpu plays, makes it the center card and removes the card from the cpus's hand
+    if(cpuSkipped == False):
+        cpuOutput = UNOlogic.cpuPlay(deck, centerCard, cpuHand, playerHand)
+        newCenter = cpuOutput[0]
+        newHand = cpuOutput[1]
+        if (newCenter != None):
+            centerCard = newCenter
+            if(UNOcard.getSpecial(newCenter) ==  ug.DRAW2):
+                print("You've Been +2'd!")
+                for num in range(0,2):
+                    playerHand = UNOlogic.draw(deck, playerHand)
+            elif(UNOcard.getSpecial(newCenter) ==  ug.WILD4):
+                print("You've Been +4'd!!")
+                for num in range(0,4):
+                    playerHand = UNOlogic.draw(deck, playerHand) 
+            elif(UNOcard.getSpecial(newCenter) == ug.SKIP or UNOcard.getSpecial(newCenter) == ug.REVERSE): 
+                    print("Your turn has been skipped!")
+                    playerSkipped = True
+        cpuHand = newHand
+    elif(cpuSkipped == True):
+        cpuSkipped = False
 
-    
-    cpuOutput = UNOlogic.cpuPlay(deck, centerCard, cpuHand, playerHand)
-    newCenter = cpuOutput[0]
-    newHand = cpuOutput[1]
-
-    if (newCenter != None):
-        centerCard = newCenter
-        if(UNOcard.getSpecial(newCenter) ==  ug.DRAW2):
-            print("You've Been +2'd!")
-            for num in range(0,2):
-                playerHand = UNOlogic.draw(deck, playerHand)
-        elif(UNOcard.getSpecial(newCenter) ==  ug.WILD4):
-            print("You've Been +4'd!!")
-            for num in range(0,4):
-                playerHand = UNOlogic.draw(deck, playerHand)     
       
-    cpuHand = newHand
+    
     
     #Game win Status Checks
     if(len(cpuHand) == 2):
